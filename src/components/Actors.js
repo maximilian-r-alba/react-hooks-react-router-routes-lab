9f87e08ba9b71e88e0f6e939a60843f187b9f3b6
Maximilian Alba <albam476@gmail.com>
2022-08-25 20:34:45 -0400

    commit tree f39261471e2dcad38134bbd0b5842899b78c2fc3

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorArray = actors.map((actor) =>{
+    const movies = actor.movies.map((movie) =>{
+      return <li key = {actor.movies.indexOf(movie)}>{movie}</li>
+    })
+    return <div key = {actors.indexOf(actor)}>
+      <h1>Name: {actor.name}</h1>
+      <p>Movies:
+       
+        
+            {movies}
+         
+       
+        
+      </p>
+    </div>
+  })
+  return <div><h1>Actors Page</h1>{actorArray}</div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorArray = directors.map((director) => {
+    const movies = director.movies.map((movie) =>{
+      return <li key = {director.movies.indexOf(movie)}>{movie}</li>
+    })
+    return <div key = {directors.indexOf(director)}>
+      <h1>Name: {director.name}</h1>
+      <p>Movies:
+       
+          {movies}
+     
+      </p>
+    </div>
+  })
+  return <div><h1>Directors Page</h1>{directorArray}</div>;
 }
 
 export default Directors;
